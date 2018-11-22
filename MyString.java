c5c73e567dea1a2799e94593a46105aff2e24d68
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-22 10:59:57 -0500

    Added exceptions for when index/start/end are negative or greater than possible

--- MyString.java
+++ MyString.java
@@ -7,6 +7,7 @@ public class MyString implements CharSequence{
     }
   }
   public char charAt(int index) {
+    if (index <0 || index >= data.length) throw new IndexOutOfBoundsException();
     return data[index];
   }
   public int length() {
@@ -15,6 +16,7 @@ public class MyString implements CharSequence{
   public CharSequence subSequence(int start, int end) {
     CharSequence a;
     String b = "";
+    if (start < 0 || end < 0 || end > length() || start > end) throw new IndexOutOfBoundsException();
     for (int i = start; i < end && i <length(); i++) {
       b += charAt(i);
     }
@@ -22,16 +24,18 @@ public class MyString implements CharSequence{
     return a;
   }
   public String toString() {
-    String ans = "";
+    String out = "";
     for (int i=0; i < this.length(); i++) {
-      ans+= this.charAt(i);
+      out += this.charAt(i);
     }
-    return ans;
+    return out;
   }
   public static void main(String[] args) {
-      MyString a = new MyString("Hello");
-    System.out.println(a.charAt(0));
-    System.out.println(a.length());
-    System.out.println(a.subSequence(0,2));
+    MyString a = new MyString("Hello");
+    System.out.println(a.charAt(0)); //H
+    System.out.println(a.length()); //5
+    System.out.println(a.subSequence(0,2)); //He
+    //System.out.println(a.charAt(-1)); // error
+    //System.out.println(a.subSequence(-1,4)); //error
     }
   }
