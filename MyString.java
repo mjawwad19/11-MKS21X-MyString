f1c187138c7bbc88ed9528fcdf662ce0cf47861e
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-22 23:24:33 -0500

    Added test cases for compareTo versus string compareTo. Looks like I need to rework my logic

--- MyString.java
+++ MyString.java
@@ -25,44 +25,44 @@ public class MyString implements CharSequence, Comparable<CharSequence>{
   }
   public String toString() {
     String out = "";
-    for (int i=0; i < this.length(); i++) {
-      out += this.charAt(i);
+    for (int i=0; i < length(); i++) {
+      out += charAt(i);
     }
     return out;
   }
   public int compareTo(CharSequence o) throws ClassCastException {
     if (o == null) throw new NullPointerException();
-    int sumData = 0;
-    for (int i = 0; i < data.length; i++) {
-      sumData += data[i];
-    }
-    for (int i = 0; i < o.length(); i++) {
-      sumData -= o.charAt(i);
-    }
-    return sumData;
+    return 0;
   }
 
-/*  public static void main(String[] args) {
+
+  public static void main(String[] args) {
       MyString first = new MyString("Hello");
       MyString second = new MyString("Hello world");
       MyString third = new MyString("Hi");
       MyString fourth = new MyString("");
       MyString fifth = new MyString("Hillo");
+      MyString sixth =  new MyString("ab");
+      MyString seventh = new MyString("ba");
       String s = null;
       System.out.println(first.charAt(0)); //H
       System.out.println(first.length()); //5
       System.out.println(first.subSequence(0,2)); //He
-      System.out.println("Hello".substring(0,2)); //He FOR CHECKING PURPOSES
-      /*System.out.println(first.subSequence(0,6)); //error
+      /*System.out.println("Hello".substring(0,2)); //He FOR CHECKING PURPOSES
+      System.out.println(first.subSequence(0,6)); //error
       System.out.println("Hello".substring(0,6)); //error FOR CHECKING PURPOSES
       System.out.println(first.charAt(-1)); // IndexOutOfBoundsException
       System.out.println(first.subSequence(-1,4)); //IndexOutOfBoundsException
-      System.out.println(first.compareTo(second)); // some negative integer
-      System.out.println(second.compareTo(first)); // opposite of previous sign
-      System.out.println(first.compareTo(third)); // some positive integer
-      System.out.println(first.compareTo(first)); // 0
-      System.out.println(first.compareTo(fourth));// VERY
-      //System.out.println(first.compareTo(s)); //NullPointerException
-      System.out.println(first.compareTo(fifth)); // some negative integer
-    }*/
+      System.out.println(first.compareTo(s)); //NullPointerException*/
+      System.out.println("ab".compareTo("ba")); //-1
+      System.out.println(sixth.compareTo(seventh));
+      System.out.println("Hello".compareTo("Hello world")); // -6
+      System.out.println(first.compareTo(second)); // -6
+      System.out.println("Hello".compareTo("Hi")); //-4
+      System.out.println(first.compareTo(third)); // -4
+      System.out.println("Hello".compareTo("")); //5
+      System.out.println(first.compareTo(fourth));// 5
+      System.out.println("Hello".compareTo("Hillo")); //-4
+      System.out.println(first.compareTo(fifth)); // -4
+    }
   }
